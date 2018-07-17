1517979df0cf333d0b86552e57e816e7cddfdc7c
debrusilva <debrusilva@gmail.com>
2018-07-17 02:24:06 +0000

    my firt commit

--- play.sh
+++ play.sh
@@ -1,74 +1,15 @@
-#!/bin/bash
-LOGFILE=/home/debru/scripts/radiodumdum.log;
-CHANNELFILE=/home/debru/radiochannels.txt
-case "$1" in
-restart|start)
+import os
+from scrapradio2 import ScrapRadio as sc
 
-if [ `ps auxwwww|grep mplayer|wc -l` -gt 0 ]
-then
-killall mplayer 2> /dev/null
-for pid in `ps auxwww|grep mplayer|tr -s ' '|cut -d ' ' -f2`
-do
-kill -9 $d 2> /dev/null
-done
+radio1 =['http://5.196.227.245:8006','Gothic Radio',17,19,'td','class','streamstats','http://5.196.227.245:8006/128mp3']
 
-fi
+sc.getDadosRadio(radio1)
 
-#mplayer mms://212.72.165.19/radiodumdum/ > /dev/null 2>&1 &
-mplayer -ao alsa http://stream.getmyip.com  >> $LOGFILE 2>&1 &
-tail -f $LOGFILE
-;;
+linkR =sc.link
 
-stop)
-if [ `ps auxwwww|grep mplayer|wc -l` -gt 0 ]
-then
-killall mplayer 2> /dev/null
-for pid in `ps auxwww|grep mplayer|tr -s ' '|cut -d ' ' -f2`
-do
-kill -9 $d 2> /dev/null
-done
-fi
-;;
-list)
-echo "Choose your interest"
-select mychannel in `cat $CHANNELFILE|cut -d ',' -f1`
-do
-if [ $mychannel ]
-then
-mymmsurl=`cat $CHANNELFILE|grep -w $mychannel|cut -d ',' -f2`;
-playlistoption=`cat $CHANNELFILE|grep -w $mychannel|cut -d ',' -f3`;
-if [ $mymmsurl ]
-then
-radio stop
-mplayer -ao alsa $playlistoption $mymmsurl >> $LOGFILE 2>&1 &
-break;
-fi
-echo "No mms URL found; continuing.. Please check file: $CHANNELFILE";
-break;
-else
-echo "Wrong choice. exiting.."
-break;
-fi
+print(linkR)
 
-done
-;;
--s)
-mychannel=$2
-$0 stop
-mymmsurl=`cat $CHANNELFILE|grep -i $mychannel|cut -d ',' -f2|head -n1`;
-playlistoption=`cat $CHANNELFILE|grep -i $mychannel|cut -d ',' -f3`;
-mplayer -ao alsa $playlistoption $mymmsurl >> $LOGFILE 2>&1 &
-;;
+#os.system('mplayer -ao alsa'+link)
 
-status)
-if [ `ps auxwwww|grep mplayer|wc -l` -gt 0 ]
-then
-tail -f -n 1 $LOGFILE
-else
-echo "radio dumdum: stopped"
-fi
-;;
-*)
-echo "Usage: $0 list|start|stop|restart"
-esac
+
 
